826e1fd2d15f39a3666f4eed4aa15c2f2c762705
Ryan Johnson <johnsonryanthomas@gmail.com>
2018-10-13 02:30:28 -0400

    Notes container

--- src/components/Notes/NotesContainer.jsx
+++ src/components/Notes/NotesContainer.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import firebase from '../../firebase';
 
 export default class NotesContainer extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: 'This is notes'
+    };
+    // setText = setText.bind(this);
+  }
+
+  componentDidMount() {
+    const slidesRef = firebase.firestore().collection('slides').doc('Lecture 9 - Wang Tiling.pdf'); //.orderByKey().limitToLast(100);
+    slidesRef.onSnapshot((slide) => {
+      this.setState({ notes: slide.data().notes });
+    });
+  }
+
+  setText(event) {
+    firebase.firestore().collection('slides').doc('Lecture 9 - Wang Tiling.pdf').set({ 'notes': event.target.value });
+  }
+
   render() {
+    const { notes } = this.state;
     return (
       <div className="NotesContainer">
         Notes Container
+        <textarea rows="4" cols="50" onInput={this.setText} value={notes} />
+
       </div>);
   }
 }
+
