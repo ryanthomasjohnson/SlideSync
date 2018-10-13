36e7d94b723ee7c41154e1b2da89914826d20715
Ryan Johnson <johnsonryanthomas@gmail.com>
2018-10-13 19:04:54 -0400

    New method for updating notes

--- src/components/Notes/NotesContainer.jsx
+++ src/components/Notes/NotesContainer.jsx
@@ -1,28 +1,40 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
 import {
-  Editor, EditorState, convertToRaw, convertFromRaw,
+  Editor, EditorState, convertToRaw, convertFromRaw, RichUtils,
 } from 'draft-js';
-// import { merge } from 'rxjs';
+import { Container } from 'semantic-ui-react';
 import firebase from '../../firebase';
+const uuidv4 = require('uuid/v4');
 
 export default class NotesContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       editorState: EditorState.createEmpty(),
+      slidesRef: firebase.firestore().collection('slides').doc('Lecture 9 - Wang Tiling.pdf'),
+      polling: false,
+      uid: uuidv4(),
     };
     this.selectionState = null;
     this.onChange = this.onChange.bind(this);
+    this._onBoldClick = this._onBoldClick.bind(this);
+    this.mergeNotes = this.mergeNotes.bind(this);
   }
 
   componentDidMount() {
-    const slidesRef = firebase.firestore().collection('slides').doc('Lecture 9 - Wang Tiling.pdf');
-    slidesRef.onSnapshot((slide) => {
-      this.setState({ notes: slide.data().notes });
+    this.state.slidesRef.onSnapshot((slide) => {
+      const last_updater = slide.data().last_updater;
+      if (last_updater == this.state.uid) {
+        return;
+      }
       const rawState = slide.data().notes;
       const contentState = convertFromRaw(rawState);
-      let editorState = EditorState.createWithContent(contentState);
+      let editorState = this.state.editorState;
+      // editorState.contentState = contentState;
+      editorState = EditorState.push(editorState, contentState);
+      // let editorState = EditorState.createWithContent(contentState);
+      // let editorState = this.state.editorState;
+      // editorState.currentContent = contentState;
       if (this.selectionState !== null) {
         editorState = EditorState.forceSelection(editorState, this.selectionState);
       }
@@ -30,22 +42,55 @@ export default class NotesContainer extends React.Component {
     });
   }
 
-  onChange(editorState) {
-    const rawState = convertToRaw(editorState.getCurrentContent());
-    const selectionState = editorState.getSelection();
+  componentWillUpdate() {
+    const selectionState = this.state.editorState.getSelection();
     this.selectionState = selectionState;
-    console.log(this.selectionState);
+  }
 
-    firebase.firestore().collection('slides').doc('Lecture 9 - Wang Tiling.pdf').set({ notes: rawState }, { merge: true });
+  onChange(editorState) {
     this.setState({ editorState });
+    if (!this.state.polling) {
+      this.setState({ polling: true });
+      const rawState = convertToRaw(editorState.getCurrentContent());
+      this.mergeNotes(rawState);
+    }
+
+
+    // const rawState = convertToRaw(editorState.getCurrentContent());
+    // const selectionState = editorState.getSelection();
+    // this.selectionState = selectionState;
+    // // this.state.slidesRef.set({ cursors: firebase.firestore.FieldValue.arrayUnion("") })
+    // this.state.slidesRef.set({ notes: rawState }, { merge: true });
+  }
+
+  mergeNotes(rawState) {
+    const { slidesRef, uid } = this.state;
+    firebase.firestore().runTransaction((transaction) => {
+      return transaction.get(slidesRef).then((slide) => {
+        transaction.set(slidesRef, { notes: rawState, last_updater: uid }, { merge: true });
+      });
+    })
+      .then(result => this.setState({ polling: false }))
+      .catch(err => {
+        this.setState({ polling: false });
+        console.log(err);
+      });
+  }
+
+  _onBoldClick() {
+    this.setState({ editorState: RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD') });
   }
 
   render() {
     const { editorState } = this.state;
     return (
       <div className="NotesContainer">
-        Notes Container
-        <Editor editorState={editorState} onChange={this.onChange} />
+        <p>Notes Container</p>
+        <button onClick={this._onBoldClick}>Bold</button>
+        <Container text>
+          <Editor editorState={editorState} onChange={this.onChange} />
+        </Container>
+
       </div>);
   }
 }
