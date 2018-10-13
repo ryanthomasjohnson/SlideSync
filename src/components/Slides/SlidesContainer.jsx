e8bf9b1cd0f3f1ba00c420d40f13e2087c5e0472
anders617 <anders617@gmail.com>
2018-10-13 14:24:24 -0400

    Added pdf to slides

--- src/components/Slides/SlidesContainer.jsx
+++ src/components/Slides/SlidesContainer.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import firebase from '../../firebase'
+import PDF from 'react-pdf-js';
+import firebase from '../../firebase';
 
 export default class SlidesContainer extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {slidePos: 0};
     const db = firebase.firestore();
     this.docRef = db.collection('slides').doc(this.props.title);
+    const storage = firebase.storage();
+    this.storageRef = storage.ref('slides/' + this.props.title);
+    this.state = {slidePos: 0, pdfUrl: null};
   }
 
   componentWillMount() {
+    this.storageRef.getDownloadURL()
+      .then((url) => {
+        this.setState({pdfUrl: url});//'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf'
+      })
+      .catch((err) => {
+        console.log(err);
+        this.setState({pdfUrl: null});
+      });
     this.docRef.onSnapshot((document) => {
       console.log('Setting slidPos to ' + document.data().slide_number);
       this.setState({slidePos: document.data().slide_number});
@@ -19,7 +30,7 @@ export default class SlidesContainer extends React.Component {
   }
 
   setSlidePos(number) {
-    this.docRef.set({slide_number: number}, {merge: true})
+    this.docRef.set({slide_number: parseInt(number)}, {merge: true})
       .then(() => {
         console.log('Set slidePos to ' + number);
       })
@@ -28,10 +39,25 @@ export default class SlidesContainer extends React.Component {
       });
   }
 
+  onDocumentLoad(pages) {
+    console.log(pages);
+  }
+
+  renderPdfDocument() {
+    console.log(this.state.pdfUrl);
+    if (this.state.pdfUrl === null) {
+      return (<div>Retrieving pdf location...</div>);
+    }
+    return (
+      <PDF file={this.state.pdfUrl} onDocumentComplete={this.onDocumentLoad} page={this.state.slidePos + 1} />
+    );
+  }
+
   render() {
     return (
-      <div className="SlidesContainer">
-        Slides Container POSITION: <input value={this.state.slidePos} onChange={(e) => {this.setSlidePos(e.target.value)}} />
+      <div className='SlidesContainer'>
+        {this.renderPdfDocument()}
+        Slides Container POSITION: <input type="number" value={this.state.slidePos} onChange={(e) => {this.setSlidePos(e.target.value)}} />
       </div>);
   }
 }
