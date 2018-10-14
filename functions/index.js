0037259b7587743341f9d2f58088272f3f652d77
Unknown <tphi@umich.edu>
2018-10-13 22:12:27 -0400

    Added initial chat functionality

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,37 @@
 const functions = require('firebase-functions');
+const bodyParser = require('body-parser');
+const cors = require('cors')({ origin: true });
+const AccessToken = require('twilio').jwt.AccessToken;
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+const ChatGrant = AccessToken.IpMessagingGrant;
+
+
+let config = {
+  accountSid: 'ACfadb650fc47f730e09bcfc240a6063b0',
+  chatServiceSid: 'IS95938c018c0b4a57b457665d394f4358',
+  apiKey: 'SKc216c19799a909a490bdaf56e8915064',
+  apiSecret: 'Y00iRXlhRS5zj8vzJMIy74ueHJxgIZxc'
+}
+
+exports.getToken = functions.https.onRequest((req, res) => {
+  return cors(req, res, () => {
+
+    const identity = req.query.identity;
+    // console.log(req);
+    // console.log(req.query.identity)
+    // console.log(req.params)
+    const accessToken = new AccessToken(config.accountSid, config.apiKey, config.apiSecret);
+    const chatGrant = new ChatGrant({
+      serviceSid: config.chatServiceSid,
+      endpointId: `${identity}:browser`
+    });
+    accessToken.addGrant(chatGrant);
+    accessToken.identity = identity;
+    res.set('Content-Type', 'application/json');
+    res.send(JSON.stringify({
+      token: accessToken.toJwt(),
+      identity: identity
+    }));
+  });
+});
---
+++ src/components/Chat/Chat.jsx
@@ -0,0 +1,201 @@
+import React, { Component } from 'react';
+import NameBox from './NameBox.js';
+import Chat from 'twilio-chat';
+
+class ChatApp extends Component {
+  constructor(props) {
+    super(props);
+    const name = localStorage.getItem('name') || '';
+
+    const loggedIn = name !== '';
+    this.state = {
+      name,
+      loggedIn,
+      token: '',
+      chatReady: false,
+      messages: [],
+      newMessage: ''
+    };
+    this.channelName = 'general';
+  }
+
+  componentWillMount = () => {
+    if (this.state.loggedIn) {
+      this.getToken();
+    }
+  };
+
+  onNameChanged = event => {
+    this.setState({ name: event.target.value });
+  };
+
+  logIn = event => {
+    event.preventDefault();
+    if (this.state.name !== '') {
+      localStorage.setItem('name', this.state.name);
+      this.setState({ loggedIn: true }, this.getToken);
+    }
+  };
+
+  logOut = event => {
+    event.preventDefault();
+    this.setState({
+      name: '',
+      loggedIn: false,
+      token: '',
+      chatReady: false,
+      messages: [],
+      newMessage: ''
+    });
+    localStorage.removeItem('name');
+    this.chatClient.shutdown();
+    this.channel = null;
+  };
+
+  getToken = () => {
+    const identityValue = encodeURIComponent(this.state.name);
+    const url = 'https://us-central1-mhacks-219303.cloudfunctions.net/getToken?identity=' +identityValue
+    // const url = 'https://us-central1-mhacks-219303.cloudfunctions.net/helloWorld'
+    // const url = 'http://localhost:3000/token/' + identityValue
+    console.log(url);
+    fetch(url, {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Access-Control-Allow-Origin':'*'
+      }
+
+    })
+    .then(data => data.json())
+    .then(data => {
+        console.log(data.token);
+        // const validToken = data.token
+        this.setState({ token:data.token }, this.initChat);
+      });
+  };
+
+//   getToken = () => {
+//   fetch(`/token/${this.state.name}`, {
+//     method: 'POST'
+//   })
+//     .then(response => response.json())
+//     .then(data => {
+//       this.setState({ token: data.token }, this.initChat);
+//     });
+// };
+
+  initChat = () => {
+    console.log("Initing chat")
+    this.chatClient = new Chat(this.state.token);
+    this.chatClient.initialize().then(this.clientInitiated.bind(this));
+  };
+
+  clientInitiated = () => {
+    this.setState({ chatReady: true }, () => {
+      this.chatClient
+        .getChannelByUniqueName(this.channelName)
+        .then(channel => {
+          if (channel) {
+            return (this.channel = channel);
+          }
+        })
+        .catch(err => {
+          if(err.body.code === 50300){
+            return this.chatClient.createChannel({
+              uniqueName: this.channelName
+            });
+          }
+        })
+        .then(channel => {
+          this.channel = channel;
+          window.channel = channel;
+          return this.channel.join();
+        })
+        .then(() => {
+          this.channel.getMessages().then(this.messagesLoaded);
+          this.channel.on('messageAdded', this.messageAdded);
+        });
+    });
+  };
+
+  messagesLoaded = messagePage => {
+    this.setState({ messages: messagePage.items });
+  };
+
+  messageAdded = message => {
+    this.setState((prevState, props) => ({
+      messages: [...prevState.messages, message]
+    }));
+  };
+
+  onMessageChanged = event => {
+    this.setState({ newMessage: event.target.value });
+  };
+
+  sendMessage = event => {
+    event.preventDefault();
+    const message = this.state.newMessage;
+    this.setState({ newMessage: '' });
+    this.channel.sendMessage(message);
+  };
+
+  newMessageAdded = li => {
+    if (li) {
+      li.scrollIntoView();
+    }
+  };
+
+  render() {
+    var loginOrChat;
+    const messages = this.state.messages.map(message => {
+      return (
+        <li key={message.sid} ref={this.newMessageAdded}>
+          <b>{message.author}:</b> {message.body}
+        </li>
+      );
+    });
+    if (this.state.loggedIn) {
+      loginOrChat = (
+        <div>
+          <h3>Messages</h3>
+          <p>Logged in as {this.state.name}</p>
+          <ul className="messages">
+            {messages}
+          </ul>
+          <form onSubmit={this.sendMessage}>
+            <label htmlFor="message">Message: </label>
+            <input
+              type="text"
+              name="message"
+              id="message"
+              onChange={this.onMessageChanged}
+              value={this.state.newMessage}
+            />
+            <button>Send</button>
+          </form>
+          <br /><br />
+          <form onSubmit={this.logOut}>
+            <button>Log out</button>
+          </form>
+        </div>
+      );
+    } else {
+      loginOrChat = (
+        <div>
+          <NameBox
+            name={this.state.name}
+            onNameChanged={this.onNameChanged}
+            logIn={this.logIn}
+          />
+        </div>
+      );
+    }
+    return (
+      <div>
+        {loginOrChat}
+      </div>
+    );
+  }
+}
+
+export default ChatApp;
