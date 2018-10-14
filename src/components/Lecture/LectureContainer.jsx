b12e17acced7675e36a58dbfc7990194d2107000
Ryan Johnson <johnsonryanthomas@gmail.com>
2018-10-14 02:44:16 -0400

    Added userlist

--- src/components/Lecture/LectureContainer.jsx
+++ src/components/Lecture/LectureContainer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Grid } from 'semantic-ui-react';
+import { Grid, Sidebar, Button } from 'semantic-ui-react';
 
 import SlidesContainer from '../Slides/SlidesContainer';
 import Editor from '../Editor/Editor';
@@ -9,29 +9,52 @@ import Chat from '../Chat/Chat';
 export default class LectureContainer extends React.Component {
   slideColRef = React.createRef();
 
-  // constructor(props) {
-  //   super(props);
-  //
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: false,
+    };
+  }
+
+  handleButtonClick = () => this.setState({ visible: !this.state.visible })
+
+  handleSidebarHide = () => this.setState({ visible: false })
 
   render() {
     const { match } = this.props;
+    const { visible } = this.state;
 
     return (
-      <div className="LectureContainer" style={ {backgroundColor: 'MintCream'} }>
-        <Grid columns={2} padded>
-          <Grid.Row>
-            <Grid.Column>
-              <div ref={this.getWidth}>
-                <SlidesContainer id={match.params.id} />
-              </div>
-              <Chat id={match.params.id} />
-            </Grid.Column>
-            <Grid.Column>
-              <Editor id={match.params.id} />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
+      <div className="LectureContainer" style={{ backgroundColor: 'MintCream' }}>
+        <Sidebar.Pushable>
+          <Sidebar
+            animation="overlay"
+            icon="labeled"
+            onHide={this.handleSidebarHide}
+            visible={visible}
+            width="wide"
+            style={{ backgroundColor: 'MintCream' }}
+          >
+            <div id="userlist" />
+          </Sidebar>
+          <Sidebar.Pusher>
+            <Grid columns={2} padded>
+              <Grid.Row>
+                <Grid.Column>
+                  <div ref={this.getWidth}>
+                    <SlidesContainer id={match.params.id} />
+                  </div>
+                  <Chat id={match.params.id} />
+                </Grid.Column>
+                <Grid.Column>
+                  <Editor id={match.params.id} showUsers={this.handleButtonClick} />
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Sidebar.Pusher>
+
+        </Sidebar.Pushable>
+
       </div>);
   }
 }
